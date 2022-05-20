4264a20b2ea9b1ea9f251561c410dc8fba2dea51
jhiney <hiney.jake@outlook.com>
2022-05-20 16:17:16 -0500

    add individual hover in footer

--- components/footer.js
+++ components/footer.js
@@ -5,13 +5,13 @@ import { IconContext } from "react-icons";
 import Link from "next/link";
 export default function Footer() {
 	return (
-		<div className="flex flex-1 justify-center items-center p-4 text-xl font-semibold content-center fixed bottom-0">
-			<div className="grid grid-cols-4 gap-4 ">
+		<div className="flex flex-1 p-4 text-xl font-semibold fixed bottom-0 right-0">
+			<div className="grid grid-cols-4 gap-4 text-gray-500 ">
 				<IconContext.Provider value={{ size: "1.5em" }}>
-					<Link href="https://github.com/jhiney" passHref={true}><VscGithub /></Link>
-					<VscMail />
-					<Link href="https://www.linkedin.com/in/jhiney/" passHref={true}><AiOutlineLinkedin /></Link>
-					<ImProfile />
+					<div className="hover:text-cyan-600"><Link  href="https://github.com/jhiney" passHref={true}><VscGithub /></Link></div>
+					<div className="hover:text-cyan-600"><VscMail /></div>
+					<div className="hover:text-cyan-600"><Link href="https://www.linkedin.com/in/jhiney/" passHref={true}><AiOutlineLinkedin /></Link></div>
+					<div className="hover:text-cyan-600"><ImProfile /></div>
 				</IconContext.Provider>
 			</div>
 		</div>
