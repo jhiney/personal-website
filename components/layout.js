4b693b6e29b21740e3c6ca674f0d4962ceda8d24
jhiney <hiney.jake@outlook.com>
2022-07-14 16:43:44 -0500

    change to prepare for upcoming login and todo page

--- components/layout.js
+++ components/layout.js
@@ -1,6 +1,6 @@
 export default function Layout({children}) {
 	return (
-		<div className="bg-white dark:bg-gradient-to-b from-slate-900 via-gray-900 to-zinc-900 text-black dark:text-white">
+		<div className="bg-white dark:bg-gradient-to-b from-slate-900 via-gray-900 to-zinc-900 text-black dark:text-white font-mono">
 		{children}	
 		</div>
 	);
