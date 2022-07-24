72454165fe770a6ab9a1c62fc0db74388dc28e27
jhiney <hiney.jake@outlook.com>
2022-07-24 10:52:02 -0500

    add supabase User Context

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,16 @@
 import Footer from "../components/footer";
 import Layout from "../components/layout";
+
+import { Auth } from "@supabase/ui";
+import { supabase } from "../lib/initSupabase";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
 	return (
 		<Layout>
-			<Component {...pageProps} />
+			<Auth.UserContextProvider supabaseClient={supabase}>
+				<Component {...pageProps} />
+			</Auth.UserContextProvider>
 			<Footer />
 		</Layout>
 	);
