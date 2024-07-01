1f1ee7ee032a2c9d73713f1848283025a72bc083
jhiney <56890661+jhiney@users.noreply.github.com>
2024-07-01 21:47:49 +0000

    update site

--- pages/about.js
+++ pages/about.js
@@ -14,8 +14,7 @@ export default function About() {
 							Hi! My name is <a className="text-cyan-500">Jake.</a>
 						</h2>
 						<p className="mt-4 text-zinc-700 dark:dark:text-zinc-300">
-							I am currently a financial systems & database analyst working in the non-profit
-							sector. Self-taught programmer trying to learn something new every day.
+							I am currently a data and software engineer supporting NASA at Johnson Space Center.
 						</p>
 
 						<dl className="mt-16 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 sm:gap-y-16 lg:gap-x-8">
@@ -34,23 +33,23 @@ export default function About() {
 
 							<div className="border-t-2 border-gray-200 pt-4 hover:border-cyan-500">
 								<dt className="text-xl font-medium dark:text-cyan-500">
-									Languages, Frameworks, and Tools
+									Languages, Frameworks, and Tools I've Used
 								</dt>
 								<dd className="mt-2 text-lg text-zinc-700  dark:text-zinc-300">
-									C# &bull; JavaScript &bull; VBA &bull; C/AL &bull; SQL &bull; HTML &bull; CSS
+									C# &bull; Go &bull; JavaScript &bull; VBA &bull; C/AL &bull; SQL &bull; HTML &bull; CSS
 								</dd>
 								<dd className="mt-2 text-lg text-zinc-700  dark:text-zinc-300">
-									React &bull; .NET &bull; Node.js &bull; Tailwind CSS &bull; Next.js
+									React &bull; Sveltekit &bull; .NET &bull; Node.js &bull; Tailwind CSS &bull; Next.js
 								</dd>
 								<dd className="mt-2 text-lg text-zinc-700  dark:text-zinc-300">
-									GIT &bull; GitHub &bull; Visual Studio &bull; SQL Server &bull; Microsoft Office
+									GIT &bull; GitHub &bull; Visual Studio &bull; Docker &bull; Postgres &bull; SQL Server &bull; Microsoft Office
 									Suite
 								</dd>
 							</div>
 
 							<div className="border-t-2 border-gray-200 pt-4 hover:border-cyan-500">
 								<dt className="text-xl font-medium dark:text-cyan-500">Currently Learning</dt>
-								<dd className="mt-2 text-lg text-zinc-700  dark:text-zinc-300">Go &bull; Rust &bull; Docker &bull; Svelte</dd>
+								<dd className="mt-2 text-lg text-zinc-700  dark:text-zinc-300">Rust &bull;</dd>
 							</div>
 						</dl>
 					</div>
